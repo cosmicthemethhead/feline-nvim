44108bf4323e81745b57f409f2fda37cde6a9b2d
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-10-02 00:41:08 +0200

    fix: removed usless options

--- lua/plugins/configs/mason.lua
+++ lua/plugins/configs/mason.lua
@@ -16,17 +16,6 @@ local options = {
       package_installed = "ﱣ ",
       package_uninstalled = " ﮊ",
     },
-
-    keymaps = {
-      toggle_server_expand = "<CR>",
-      install_server = "i",
-      update_server = "u",
-      check_server_version = "c",
-      update_all_servers = "U",
-      check_outdated_servers = "C",
-      uninstall_server = "X",
-      cancel_installation = "<C-c>",
-    },
   },
 
   max_concurrent_installers = 10,
